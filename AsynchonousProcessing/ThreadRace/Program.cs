9676314b528f81646aaefa8ac9007d7cd60d62bb
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-04-23 12:24:23 +0300

    Adding example for Thread Race and solution with lock.

---
+++ AsynchonousProcessing/ThreadRace/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object num = new object();
+            List<int> numbers = Enumerable.Range(0, 1000000).ToList();
+            for (int i = 0; i < 4; i++)
+            {
+                new Thread(() =>
+                {
+                    lock (num)
+                    {
+                        while (numbers.Count > 0)
+                        {
+
+                            numbers.RemoveAt(numbers.Count - 1);
+                        }
+
+                    }
+                }).Start();
+
+                Console.WriteLine("Success!!!");
+            }
+        }
+    }
+}
