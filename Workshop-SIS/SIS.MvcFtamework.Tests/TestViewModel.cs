907cc265772948c21b7e8775787cb42dafb8c720
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-05-06 00:05:58 +0300

    Refactoring ViewEngineTests

---
+++ Workshop-SIS/SIS.MvcFtamework.Tests/TestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS.MvcFtamework.Tests
+{
+   
+        public class TestViewModel
+        {
+            public string Name { get; set; }
+
+            public decimal Price { get; set; }
+
+            public DateTime DateOfBirth { get; set; }
+        }
+}
